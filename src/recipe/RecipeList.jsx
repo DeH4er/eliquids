688e5573757c805a7d7f101fde7f6f69f5028a6b
Denys Lytviak <deh4er@gmail.com>
2021-05-18 17:53:39 +0200

    up

--- src/recipe/RecipeList.jsx
+++ src/recipe/RecipeList.jsx
@@ -46,42 +46,40 @@ function RecipeListContainer() {
         }
       />
 
-      <VStack spacing="4" flex="1" overflow="auto">
-        <VStack
-          flex="1"
-          overflow="auto"
-          width="full"
-          paddingX="4"
-          paddingBottom="4"
-        >
-          {recipes.map((recipe) => (
-            <VStack
-              key={recipe.id}
-              width="full"
-              padding="4"
-              borderColor="gray.600"
-              borderWidth="1px"
-              borderRadius="4"
-              spacing="4"
-              onClick={() => onRecipeClick(recipe)}
-            >
-              <Flex justifyContent="space-between" width="full">
-                <Heading size="md">{recipe.name}</Heading>
-                <Text opacity=".8">
-                  {recipe.vg}/{recipe.pg}
-                </Text>
-              </Flex>
+      <VStack
+        flex="1"
+        overflow="auto"
+        width="full"
+        paddingX="4"
+        paddingBottom="4"
+      >
+        {recipes.map((recipe) => (
+          <VStack
+            key={recipe.id}
+            width="full"
+            padding="4"
+            borderColor="gray.600"
+            borderWidth="1px"
+            borderRadius="4"
+            spacing="4"
+            onClick={() => onRecipeClick(recipe)}
+          >
+            <Flex justifyContent="space-between" width="full">
+              <Heading size="md">{recipe.name}</Heading>
+              <Text opacity=".8">
+                {recipe.vg}/{recipe.pg}
+              </Text>
+            </Flex>
 
-              <Wrap width="full">
-                {recipe.flavors.map((flavor) => (
-                  <Tag key={flavor.flavor.id}>
-                    {flavor.flavor.name} ({flavor.percent}%)
-                  </Tag>
-                ))}
-              </Wrap>
-            </VStack>
-          ))}
-        </VStack>
+            <Wrap width="full">
+              {recipe.flavors.map((flavor) => (
+                <Tag key={flavor.flavor.id}>
+                  {flavor.flavor.name} ({flavor.percent}%)
+                </Tag>
+              ))}
+            </Wrap>
+          </VStack>
+        ))}
       </VStack>
     </>
   );
